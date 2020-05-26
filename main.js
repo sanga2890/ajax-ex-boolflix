a40b4694aeba09f4e06463536008a82e81568a7a
Stefano <stefano2890@gmail.com>
2020-05-26 16:30:22 +0200

    add some comments

--- main.js
+++ main.js
@@ -8,30 +8,54 @@
 
 // aggancio il click al bottone per eseguire la ricerca;
 $('.button-search').click(function(){
+
     // pulisco la pagina da risultati pecedenti;
     $('.results').text('')
 
-    // vado a leggere quello che l'utente ha scritto nella barra di ricerca e lo salvo in una variabile;
-    var input_utente = $('.input-search').val().toLowerCase();
 
-    // richiamo le funzioni per richiedere i dati all'API di Tmdb;
-    richiesta_dati(input_utente)
-    richiesta_serie(input_utente)
+    // vado a leggere quello che l'utente ha scritto nella barra di ricerca e lo salvo in una variabile;
+    var input_utente = $('.input-search').val().trim().toLowerCase();
+
+    // verifico che siano stati inseriti almeno 2 caratteri;
+    if (input_utente.length > 1 ){
+
+        // richiamo le funzioni per richiedere i dati all'API di Tmdb;
+        richiesta_dati(input_utente)
+        richiesta_serie(input_utente)
+    } else {
+
+      // avviso che indica il numero minimo di caratteri da digitare;
+      alert('Caratteri insufficienti! Inserisci almeno 2 lettere per poter effettuare una ricerca.');
+  }
+
+
 
 });
 
-$('.input-search').keypress(function (e) {
+$('.input-search').keyup(function (e) {
     // pulisco la pagina da risultati pecedenti;
     $('.results').text('')
 
+
     // vado a leggere quello che l'utente ha scritto nella barra di ricerca e lo salvo in una variabile;
-    var input_utente = $('.input-search').val().toLowerCase();
+    var input_utente = $('.input-search').val().trim().toLowerCase();
 
   if (e.which == 13)  {
-      // richiamo le funzioni per richiedere i dati all'API di Tmdb;
-      richiesta_dati(input_utente)
-      richiesta_serie(input_utente)
+
+      // verifico che siano stati inseriti almeno 2 caratteri;
+      if (input_utente.length > 1 ){
+
+          // richiamo le funzioni per richiedere i dati all'API di Tmdb;
+          richiesta_dati(input_utente)
+          richiesta_serie(input_utente)
+        } else {
+
+        // avviso che indica il numero minimo di caratteri da digitare;
+        alert('Caratteri insufficienti! Inserisci almeno 2 lettere per poter effettuare una ricerca.');
+    }
+
   }
+
 });
 
 // creo una funzione per far partire la chiamata ajax al database di TMDB per richiamare l'API contenente le info dei film;
@@ -45,6 +69,7 @@ function richiesta_dati(input){
             },
             'success': function(answer) {
                 var movies = answer.results
+
                 // inseriscon la funzione per ciclare i film in base al risultato della ricerca;
                 gestione_dati(movies)
             },
@@ -53,10 +78,12 @@ function richiesta_dati(input){
         }
     });
 }
+
 // funzione per ciclare tutti i film trovati;
 function gestione_dati(film) {
     for (var i = 0; i < film.length; i++) {
         var current_movie = film[i]
+
         // inserisco la funzione per la stampa in pagina;
         stampa_movies(current_movie)
     }
@@ -64,25 +91,33 @@ function gestione_dati(film) {
 
 // funzione per stampare in pagina solo le informazioni che mi interessano per ciascun film trovato
 function stampa_movies(info) {
+
     // faccio diventare il voto da 1 a 5 al posto di 1 a 10 e arrotondo;
     var rounded = Math.round(info.vote_average / 2);
+
     // uso handlebars per clonare il template
     var source = $("#movie-template").html();
         var template = Handlebars.compile(source);
         var context = {
+
             // recupero la locandina;
             'img' : 'https://image.tmdb.org/t/p/w342' + info.backdrop_path,
+
             // recupero il titolo;
             'titolo' : info.title,
+
             // recupero il titiolo originale;
             'titolo_originale' : info.original_title,
+
             // recupero la lingua originale;
             'lingua' :flags(info.original_language),
+
             // recupero il voto arrotondato;
             'voto' : rating_stars(rounded)
         }
 
         var html = template(context);
+
         // stampo tutto in pagina nell'apposito container;
         $('.results').append(html);
 }
@@ -119,34 +154,48 @@ function gestione_dati_serie(serie) {
 }
 
 function stampa_series(info) {
+
     // faccio diventare il voto da 1 a 5 al posto di 1 a 10 e arrotondo;
     var rounded = Math.round(info.vote_average / 2);
+
     // uso handlebars per clonare il template
     var source = $("#serie-template").html();
         var template = Handlebars.compile(source);
         var context = {
+
             // recupero la locandina;
             'img' : 'https://image.tmdb.org/t/p/w342' + info.backdrop_path,
+
             // recupero il titolo;
             'titolo' : info.name,
+
             // recupero il titiolo originale;
             'titolo_originale' : info.original_name,
+
             // recupero la lingua originale;
             'lingua' :flags(info.original_language),
+
             // recupero il voto arrotondato;
             'voto' : rating_stars(rounded)
         }
 
         var html = template(context);
+
         // stampo tutto in pagina nell'apposito container;
         $('.results').append(html);
 }
 
+// funzione per sostitutire la lingua con la rispettiva bandiera;
 function flags(lingua) {
+
+    // recupero il template con handlebars;
     var source = $("#flag-template").html();
-        var template = Handlebars.compile(source);
+    var template = Handlebars.compile(source);
+
+    // dichiaro una variabile con un array contenente le immagini delle bandiere che ho a disposizione;
     var bandiere = ['en', 'it', 'de', 'fr', 'es'];
 
+    // verifico che la lingua inserita sia presente nell'array bandiere;
     if (bandiere.includes(lingua)){
         var context = {
             'flag' : lingua
@@ -161,12 +210,21 @@ function flags(lingua) {
     return lingua;
 };
 
+// funzione per sositire il voto con delle stelline;
 function rating_stars(rating) {
+
+    // dichiaro una variabile vuota;
     var stars = '';
+
+    // ciclo per 5 volte (la votazione va da 1 a 5);
     for (var i = 0; i < 5; i++) {
+
+        // in base al numero che sta ad indicare il voto vado a stampare un numero di stelline piene, pari al voto recuperato dall'api;
         if (i <= rating) {
             stars += '<i class="fas fa-star"></i>';
         } else {
+
+            // se il voto è inferiore a 5, stampo le restanti stelline vuote;
             stars += '<i class="far fa-star"></i>';
         }
     }
